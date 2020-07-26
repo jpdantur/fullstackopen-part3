ce06a79e89c92400a66de23d3c2e0819a5f5777c
jpdantur <jpdantur@gmail.com>
2020-07-25 22:08:12 -0300

    uibuild

---
+++ build/precache-manifest.e4bb0d9a4d5078320b513a530fe4e12d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e504bedaf2f5f0e1356c4032395e064a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c7a5a37188e700465a8",
+    "url": "/static/css/main.eb82856b.chunk.css"
+  },
+  {
+    "revision": "c5cf8bd149580aa910cd",
+    "url": "/static/js/2.57bf6790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57bf6790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7a5a37188e700465a8",
+    "url": "/static/js/main.e295963c.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
