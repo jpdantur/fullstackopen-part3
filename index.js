a16f78a807207a9039defa4a817db040220a839d
jpdantur <jpdantur@gmail.com>
2020-07-28 23:59:39 -0300

    3.18

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((result) => {
     res.json(result);
